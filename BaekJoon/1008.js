2631d11e73e6abb19c1137bf34a2c3d5b952a9b7
korthj <khj12005@gmail.com>
2022-03-29 22:41:58 +0900

    1008번 문제

---
+++ BaekJoon/1008.js
@@ -0,0 +1,3 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ");
+// const input = require('fs').readFil/eSync('./testInput.txt').toString().trim().split(" ");
+console.log(input[0]/input[1]);
