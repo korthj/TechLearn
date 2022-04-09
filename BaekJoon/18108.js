b414f4232f1ea11e642951678e6d6d4ee20a2059
korthj <khj12005@gmail.com>
2022-04-09 23:11:52 +0900

    18108번 문제

---
+++ BaekJoon/18108.js
@@ -0,0 +1,4 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split();
+const input = require('fs').readFileSync('./testInput.txt').toString().trim().split();
+
+console.log(Number(input[0])-543);
