5a6088ab34b4940648fa79b6f7744a5031ba5dcc
korthj <khj12005@gmail.com>
2022-04-11 14:04:09 +0900

    같은 숫자는 싫어 문제

---
+++ programmers/같은숫자는싫어.js
@@ -0,0 +1,13 @@
+const arr =[1,1,3,3,0,1,1];
+
+function solution(arr){
+    const result = [];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== arr[i+1]){
+            result.push(arr[i]);
+        };
+    };
+    return result;
+}
+
+console.log(solution(arr));
