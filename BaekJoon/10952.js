93cef10823c3aa42f04bd39747ccaef6f60a0053
korthj <khj12005@gmail.com>
2022-05-05 22:36:14 +0900

    10952번 문제

---
+++ BaekJoon/10952.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync('./testInput.txt').toString().trim().split('\n').map(v=>v.split(' ').map(Number));
+let result = '';
+for(let i=0;i<input.length-1;i++)result += (input[i][0]+input[i][1])+'\n';
+console.log(result);
