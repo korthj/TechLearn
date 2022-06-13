ccb07a7b06c34e80141b0ad6058eb884e7aca6cf
korthj <khj12005@gmail.com>
2022-06-13 21:11:33 +0900

    5339번 문제

---
+++ BaekJoon/5339.js
@@ -0,0 +1,14 @@
+let answer = '';
+
+answer += `     /~\\\n`;
+answer += `    ( oo|\n`;
+answer += `    _\\=/_\n`;
+answer += `   /  _  \\\n`;
+answer += `  //|/.\\|\\\\\n`;
+answer += ` ||  \\ /  ||\n`;
+answer += `============\n`;
+answer += `|          |\n`;
+answer += `|          |\n`;
+answer += `|          |`
+
+console.log(answer);
