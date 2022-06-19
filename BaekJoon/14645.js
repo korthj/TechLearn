e6cadcc119518605be00f11e9401cefb31c72fcf
korthj <khj12005@gmail.com>
2022-06-19 22:31:53 +0900

    14645번 문제

---
+++ BaekJoon/14645.js
@@ -0,0 +1,6 @@
+// const input = require("fs").readFileSync("testInput.txt").toString().split("\n").map(v => v.split(" ").map(Number));
+
+// for(let i = 0; i < input.length; i++) {
+//     if(i === input.length-1)console.log('비와이');
+// };
+console.log('비와이');
