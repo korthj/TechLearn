a821367339fb1607f14e83ecbc08ab488398a9e9
korthj <khj12005@gmail.com>
2022-08-10 21:44:36 +0900

    2446번 문제

---
+++ BaekJoon/2446.js
@@ -0,0 +1,17 @@
+const input = require("fs").readFileSync("testInput.txt").toString();
+let star = "*".repeat(2 * Number(input) - 1).split("");
+let start = -1; // ++
+let end = 2 * Number(input) - 1; // --
+let result = star.join("") + "\n";
+
+for (let i = 1; i < Number(input); i++) {
+	start++;
+	end--;
+	star[start] = " ";
+	star[end] = "";
+	result += star.join("") + "\n";
+}
+
+console.log(result.trim());
+let result2 = result.split("\n").slice(0, -2).reverse();
+console.log(result2.join("\n"));
