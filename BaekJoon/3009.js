dad28255e2f5cd26f7c30fe718739d66128a7a30
korthj <khj12005@gmail.com>
2022-08-13 00:23:05 +0900

    3009번문제

---
+++ BaekJoon/3009.js
@@ -0,0 +1,16 @@
+const input = require("fs").readFileSync("testInput.txt").toString().split("\n");
+let arrayX = [];
+let arrayY = [];
+let x;
+let y;
+for(let i = 0; i < 3; i++){
+    arrayX.push(Number(input[i].split(" ")[0]));
+    arrayY.push(Number(input[i].split(" ")[1]));
+}
+arrayX = arrayX.sort();
+arrayy = arrayY.sort();
+
+x = arrayX[1] === arrayX[0] ? arrayX[2] : arrayX[0];
+y = arrayY[1] === arrayY[0] ? arrayY[2] : arrayY[0];
+
+console.log(`${x} ${y}`);
