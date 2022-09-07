cefb5eb5aeb0c59221967c2b186067b6cdc55195
korthj <khj12005@gmail.com>
2022-09-07 22:22:37 +0900

    5086번문제

---
+++ BaekJoon/5086.js
@@ -0,0 +1,13 @@
+const input = require("fs").readFileSync("testInput.txt").toString().trim().split("\n");
+input.pop();
+const nums = input.map(v => v.split(' ').map(x => +x))
+
+nums.forEach(v => {
+  if (v[1] % v[0] == 0) {
+    console.log('factor')
+  } else if (v[0] % v[1] == 0) {
+    console.log('multiple')
+  } else {
+    console.log('neither')
+  }
+})
