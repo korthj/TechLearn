4983194cace92eac85905c217bd555b363d1610e
korthj <khj12005@gmail.com>
2022-10-13 22:10:23 +0900

    10699번 문제

---
+++ BaekJoon/10699.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.time.*;
+
+public class Main {
+	public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+	};
+};
