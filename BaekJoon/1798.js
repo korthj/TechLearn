56811e81bc353e0c6573df6008df52554147bcf0
korthj <khj12005@gmail.com>
2022-12-04 14:59:37 +0900

    1798번문제

---
+++ BaekJoon/1798.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync("/dev/stdin").toString();
+let sum = 0, cnt = 0;
+
+for(let i =1;; i++){
+    sum += i
+    cnt++;
+    if(sum > input){
+        cnt--;
+        break;
+    };
+};
+
+console.log(cnt);
