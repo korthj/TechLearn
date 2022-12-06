bad225fb1bb898d37f518c3c8ff19f3344997d9f
korthj <khj12005@gmail.com>
2022-12-06 17:25:50 +0900

    1439번문제

---
+++ BaekJoon/1439.js
@@ -0,0 +1,28 @@
+const input = require('fs').readFileSync("testInput.txt").toString().trim().split('');
+let setNumber = 0, answer_cnt1 = 0, answer_cnt2 = 0;;
+
+for(let i = 0; i < input.length; i++){
+    if(setNumber !== parseInt(input[i])){
+        if(i===0){
+            answer_cnt1++;
+        }else{
+            if(input[i] !== input[i - 1]){
+                answer_cnt1++;
+            };
+        };
+    };
+};
+setNumber = 1;
+for(let i = 0; i < input.length; i++){
+    if(setNumber !== parseInt(input[i])){
+        if(i===0){
+            answer_cnt2++;
+        }else{
+            if(input[i] !== input[i - 1]){
+                answer_cnt2++;
+            };
+        };
+    };
+};
+
+console.log(Math.min(answer_cnt1,answer_cnt2));
