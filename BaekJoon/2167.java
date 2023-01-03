4269400d4998458c0d3db77d82c1e3c546807e50
korthj <khj12005@gmail.com>
2023-01-03 12:26:49 +0900

    2167번 문제

---
+++ BaekJoon/2167.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+ 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            };
+        };
+ 
+        int k = Integer.parseInt(br.readLine());
+        
+        while (k-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+ 
+            int sum = 0;
+            for (int a = i - 1; a < x; a++) {
+                for (int b = j - 1; b < y; b++) {
+                    sum += arr[a][b];
+                };
+            };
+            System.out.println(sum);
+        };
+    };
+};
