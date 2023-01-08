42a64e06b7566856a5266b4d9f76f33b3708af3b
korthj <khj12005@gmail.com>
2023-01-08 22:00:01 +0900

    24461번 문제

---
+++ BaekJoon/24416.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('testInput.txt').toString().trim();
+
+const solution = (input) => {
+  const n = +input;
+  const dp = new Array(n + 1).fill(0);
+  let cnt = 0;
+
+  dp[1] = dp[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+    cnt++;
+  };
+
+  return [dp[n], cnt].join(" ");
+};
+
+console.log(solution(input));
