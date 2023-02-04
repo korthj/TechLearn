e4157e50f2ccb376433145a3d9faf1841f2881a8
korthj <khj12005@gmail.com>
2023-02-04 15:00:56 +0900

    2828번 문제 주석 추가

--- BaekJoon/2828.js
+++ BaekJoon/2828.js
@@ -7,12 +7,13 @@ appleCoordinate.shift();
 
 let move = 0;
 let left = 0;
-let right = 0 + (basketSize - 1);
+let right = 0 + (basketSize - 1); //바구니 크기에서 -1을 한 값이 left와 같다면, 바구니 크기는 1칸 짜리임을 의미
 
 for(let i = 0; i < appleCoordinate.length; i++) {
-    //
+    //현재 사과가 떨어지는 위치에서 -1을 하는 이유는 현재 바구니의 위치 고려.
     let tmp = appleCoordinate[i] - 1;
 
+    //하여 사과의 낙하 위치가 바구니의 범위 안이라면 위치 변동은 없음. 그 외 경우 바구니의 현재 위치만 수정.
     if(tmp > right) {
         move += tmp - right;
         right = tmp;
