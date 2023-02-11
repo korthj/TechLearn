c7f0bf9c469d8ed9463b6a02b497456e80adfa31
korthj <khj12005@gmail.com>
2023-02-11 14:24:22 +0900

    10845번 문제

---
+++ BaekJoon/10845.js
@@ -0,0 +1,29 @@
+//10845번 큐 문제, 스위치문을 사용하여 풀었고 결과는 다른 이들에 비해 속도는 조금 더 빠르지만 메모리를 조금 더 많이 사용한다.
+let [ N,...input ] = require("fs").readFileSync("testInput.txt").toString().trim().split("\n").map(v => v.split(" "));
+
+const queue = [];
+let result = '';
+for(let i = 0; i < N; i++) {
+    //
+    switch(input[i][0]){
+        case "push":
+            queue.push(input[i][1]);
+            break;
+        case "pop":
+            result += (queue.length === 0 ? "-1" : queue.shift()) + "\n";
+            break;
+        case "size":
+            result += (queue.length) + "\n";
+            break;
+        case "empty":
+            result += (queue.length === 0 ? "1" : "0") + "\n";
+            break;
+        case "front":
+            result += (queue.length === 0 ? "-1" : queue[0]) + "\n"; 
+            break;
+        case "back":
+            result += (queue.length === 0 ? "-1" : queue[queue.length - 1]) + "\n";
+            break;
+    };    
+};
+console.log(result);
