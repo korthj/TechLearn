5e62a9b760ba61813182285545e324a7c2fed686
korthj <khj12005@gmail.com>
2023-02-25 23:12:55 +0900

    1748번 문제

---
+++ BaekJoon/1748.js
@@ -0,0 +1,19 @@
+//1748번 수 이어 쓰기 1 문제
+const filePath = process.platform === "linux" ? "/dev/stdin" : "testInput.txt";
+const input = +require("fs").readFileSync(filePath).toString().trim();
+
+let answer = 0;
+let num = 1;
+let d = 10
+let s = 1;
+while(num<=input){
+  if(Math.floor(num/d)==0){
+    num++;
+    answer+=s;
+  }else{
+    d*=10;
+    s++;
+  };
+};
+
+console.log(answer);
