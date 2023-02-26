258b9554fbac730fb3284835d0df8071eb0d9529
korthj <khj12005@gmail.com>
2023-02-26 12:17:45 +0900

    1748번 문제 수정

--- BaekJoon/1748.js
+++ BaekJoon/1748.js
@@ -2,18 +2,18 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "testInput.txt";
 const input = +require("fs").readFileSync(filePath).toString().trim();
 
-let answer = 0;
+let result = 0;
 let num = 1;
 let d = 10
 let s = 1;
 while(num<=input){
   if(Math.floor(num/d)==0){
     num++;
-    answer+=s;
+    result+=s;
   }else{
     d*=10;
     s++;
   };
 };
 
-console.log(answer);
+console.log(result);
