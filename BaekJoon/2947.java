7c4da1b8e36c87e843fb60f9df67b22f1b6cc93e
korthj <khj12005@gmail.com>
2023-04-04 21:47:11 +0900

    2947번 문제

---
+++ BaekJoon/2947.java
@@ -0,0 +1,26 @@
+//2947번 나무 조각 문제
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //주어진 숫자 입력받기
+        int[] nums = new int[5];
+        for (int i = 0; i < 5; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for (int j=0;j<5;j++){
+            for (int i = 0; i < 4; i++) {
+                if(nums[i]>nums[i+1]){
+                    int tmp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = tmp;
+                    for (int num : nums) {
+                        System.out.print(num+" ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
